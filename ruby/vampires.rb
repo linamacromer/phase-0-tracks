7c58cda95f0dd65ca9236e0dbec51071b3aba337
Lina Cromer <linamacromer@gmail.com>
2016-08-01 11:06:01 -0700

    Add psuedocode for Vampire test logic

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -8,4 +8,23 @@ puts "Our company cafeteria serves garlic bread. Should we order some for you? T
 garlic = gets.chomp
 puts "Would you like to enroll in the company's health insurance? Type Yes or No"
 health = gets.chomp
-puts "To recap, you are #{name}, #{age} ys old, born in #{birthyear}, #{garlic} to garlic, and #{health} to health insurance."
+
+#Check if name is Drake Cula or Tu Fang, vampire_name is true
+
+#Check if age is wrong, vampire_age is true
+
+#Check if garlic answer was no, vampire_garlic is true
+
+#Check if health insurance answer was no, vampire_health is true
+
+#If age=false AND (garlic OR health)=false, "probably not a vampire"
+
+#If age=true AND (garlic OR health =true), "probably a vampire"
+
+#If age=true AND garlic=true AND health=true, "almost certainly a vampire"
+
+#If age=false, garlic=false, health=false, name=true, "Definitely a vampire"
+
+#Else "Results inconclusive"
+
+#print results
