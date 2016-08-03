36a0cb2441326bb5f592f6b251ee8c6d1ff03dce
Lina Cromer <linamacromer@gmail.com>
2016-08-03 16:33:25 -0700

    Add pseudocode to encrypt and decrypt methods

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,13 @@
+#First method: encrypt
+	#advances every letter of string one letter forward, spaces remain space
+#walk through string
+	#for each letter, use .next method
+
+
+
+#Second method: decrypt
+	#reverses above
+
+#walk through string
+	#look at each letter, determine what number of index it is
+	#change it to the previous index number character
