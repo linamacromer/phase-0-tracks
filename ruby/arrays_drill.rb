0afe903f85731d02a0fe78de649a2e8c586ad33d
Lina Cromer <linamacromer@gmail.com>
2016-08-08 13:37:43 -0700

    Add code for Release 1

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,2 +1,20 @@
 test_array = []
 p test_array
+
+test_array << "red" << "blue" << "green" << "yellow" << "purple"
+p test_array
+
+test_array.delete_at(2)
+p test_array
+
+test_array.insert(2, "orange")
+p test_array
+
+test_array.delete("red")
+p test_array
+
+puts "Does the array include 'purple'? #{test_array.include?("purple")}"
+
+test_array2 = ["apple", "banana", "cantaloupe"]
+test_array3 = test_array + test_array2
+p test_array3
