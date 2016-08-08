c5e8f7628e7431279b2ecc3aa929195cbc9cc1ff
Lina Cromer <linamacromer@gmail.com>
2016-08-08 13:45:08 -0700

    Add code for Release 2

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -1,3 +1,11 @@
+def build_array(x,y,z)
+	new_array = [x,y,z]
+end
+
+def add_to_array(array,x)
+	array << x
+end	
+
 test_array = []
 p test_array
 
@@ -18,3 +26,12 @@ puts "Does the array include 'purple'? #{test_array.include?("purple")}"
 test_array2 = ["apple", "banana", "cantaloupe"]
 test_array3 = test_array + test_array2
 p test_array3
+
+new_array = build_array(1,"two",nil)
+p new_array
+
+add_to_array(new_array,"a")
+p new_array
+
+add_to_array(new_array,"b")
+p new_array
