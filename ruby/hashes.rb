520ee7e57473469a12000767144e94637c4e0c0d
Lina Cromer <linamacromer@gmail.com>
2016-08-09 20:24:38 -0700

    Add user interface questions

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -6,6 +6,18 @@
 	#Print updated hash back out to screen as final
 
 #Business Logic
-
+client = {}
 
 #User Interface
+print "Hello, please input client information.\nName: "
+client[:name] = gets.chomp
+print "Age: "
+client[:age] = gets.chomp.to_i
+print "Number of Children: "
+client[:children] = gets.chomp.to_i
+print "Favorite color: "
+client[:color] = gets.chomp
+print "Hometown: "
+client[:hometown] = gets.chomp
+print "Do you have any pets? (Y/N): "
+client[:pets] = gets.chomp
