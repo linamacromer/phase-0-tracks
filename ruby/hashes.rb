448318066c60d2f7607385aff4eef459d188fa9d
Lina Cromer <linamacromer@gmail.com>
2016-08-09 20:14:36 -0700

    Initial commit, add pseudocode

---
+++ ruby/hashes.rb
@@ -0,0 +1,11 @@
+#Pseudocode
+	#Prompt user for all of the info
+	#Convert any user input to appropriate data type
+	#Print hash back out to review when all questions answered
+	#Give user opportunity to udpdate a key, or none.
+	#Print updated hash back out to screen as final
+
+#Business Logic
+
+
+#User Interface
