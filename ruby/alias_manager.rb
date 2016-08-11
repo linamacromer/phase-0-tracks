019751a308050fd7a85983b52a4c9e7417c88ae1
Lina Cromer <linamacromer@gmail.com>
2016-08-10 21:03:21 -0700

    Add logic to swap first and last name to create new name

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -2,3 +2,13 @@
 #Swap First and Last name
 #Change aeiou to next aeiou
 #Change consonants to next consonant
+
+first = "Felicia"
+last = "Torres"
+real_name = first + " " + last
+new_name = ""
+
+def first_last_swap(old,new)
+	new = old.split(" ").reverse!.join(" ")
+end
+p first_last_swap(real_name,new_name)
