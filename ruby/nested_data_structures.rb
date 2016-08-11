2f6e687435ae596d89ee85112e5ca176d1ce6dad
Lina Cromer <linamacromer@gmail.com>
2016-08-10 20:24:57 -0700

    Complete nested data structure of house

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -16,11 +16,84 @@ house = {
 			total_switches: 8
 			current_switches_on: 6
 		}
+	},
+	kitchen: {
+		wall_color: "red"
+		furniture: [
+			"refridgerator",
+			"toaster oven",
+			"coffee maker",
+			"microwave"
+		]
+		decoration: [
+			"cutting board",
+			"wine rack",
+			"flower vase"
+		]
+		lights: {
+			total_switches: 4
+			current_switches_on: 2
+		}
+	},
+	garage:{
+		wall_color: "white"
+		furniture: [
+			"cars",
+			"workbench",
+			"tool box",
+			"lawnmower"
+		]
+		decoration: []
+		lights: {
+			total_switches: 3
+			current_switches_on: 0
+		}
+	},
+	bedroom:{
+		wall_color: "blue"
+		furniture: [
+			"bed",
+			"dresser",
+			"armoire",
+			"end table"
+		]
+		decoration: [
+			"family portrait",
+			"string lights",
+			"ornate mirror"
+		]
+		lights: {
+			total_switches: 2
+			current_switches_on: 0
+		}
+	},
+	bathroom:{
+		wall_color: "green"
+		furniture: [
+			"towel basket"
+		]
+		decoration: [
+			"clock",
+			"fancy soap",
+			"candle"
+		]
+		lights: {
+			total_switches: 2
+			current_switches_on: 0
+		}
+	},
+	closet:{
+		wall_color: "white"
+		furniture: [
+			"washer",
+			"dryer",
+			"laundry hamper",
+			"linens"
+		]
+		decoration: []
+		lights: {
+			total_switches: 1
+			current_switches_on: 1
+		}
 	}
-	kitchen: 
-	garage:
-	bedroom:
-	bathroom:
-	closet:
-	den:
 }
