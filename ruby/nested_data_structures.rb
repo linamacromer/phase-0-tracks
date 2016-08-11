36d5b79c313a6b4ef5303f1cbfc1a93f52cb6f9b
Lina Cromer <linamacromer@gmail.com>
2016-08-10 20:14:13 -0700

    Add outline structure of house and nested structures

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,26 @@
+house = {
+	living_room: {
+		wall_color: "beige"
+		furniture: [
+			"couch",
+			"television",
+			"coffee table",
+			"thermostat"
+		]
+		decoration: [
+			"cave painting",
+			"wedding photo",
+			"grandfather clock"
+		]
+		lights: {
+			total_switches: 8
+			current_switches_on: 6
+		}
+	}
+	kitchen: 
+	garage:
+	bedroom:
+	bathroom:
+	closet:
+	den:
+}
