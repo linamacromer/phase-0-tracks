b168530af22d360b5bbbab120ad5ff632019014f
Lina Cromer <linamacromer@gmail.com>
2016-08-11 17:03:22 -0700

    Add method to find vowels and output the next vowel

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -7,16 +7,37 @@ first = "Felicia"
 last = "Torres"
 real_name = first + " " + last
 new_name = ""
+name_array = []
 
 def first_last_swap(old,new)
 	new = old.split(" ").reverse!.join(" ")
 end
 new_name = first_last_swap(real_name,new_name)
-p new_name
+
 #Split the name into all characters. Loop for all letters, if it is a vowel, find the index and increase by one. 
 
-def vowels (name)
+def letter_array (name)
 	name.chars
 end
 
-p vowels(new_name)
+# Figure out what index it matches in the vowels, increase that by one, and replace it in the array with that. Do that for each item in the array.
+
+def vowel_change(letter)
+	vowels = ["a", "e", "i", "o", "u"]
+
+	if vowels.include? letter
+		p key = vowels.index(letter)
+		p letter = vowels[key+1]
+		else
+	end
+end
+
+name_array = letter_array(new_name)
+
+name_array.each do |x|
+	p vowel_change(x)
+end
+
+p name_array
+#letter_array(new_name).each do |variable|
+#end
