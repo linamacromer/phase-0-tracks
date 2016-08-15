26a4f05c0fc54d6d842754be3061c47769a12892
Lina Cromer <linamacromer@gmail.com>
2016-08-15 15:33:02 -0700

    Add logic in initializing method to print gender/ethnicity/age for review, using instance methods

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -9,6 +9,8 @@ class Santa
 		@ethnicity = ethnicity
 		#@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
 		@age = rand(140)
+		puts "This Santa is: \nGender: #{@gender} \nEthnicity: #{@ethnicity} \nAge: #{@age}"
+		puts "----------"
 	end
 
 	def speak
@@ -37,11 +39,9 @@ end
 # Nick.eat_milk_and_cookies(cookie_type)
 
 santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders = ["agender", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Asian", "prefer not to say", "N/A"]
 
-until santas.length == 50
+until santas.length == 5
   santas << Santa.new(example_genders.sample, example_ethnicities.sample)
 end
-
-p santas
