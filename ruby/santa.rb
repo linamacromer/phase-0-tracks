d5d869ddf77fc31031ba431416f75bcccda782e5
Lina Cromer <linamacromer@gmail.com>
2016-08-15 11:18:10 -0700

    Move initialize method to beginning, add attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,15 +1,19 @@
 class Santa
 
+	def initialize(gender,ethnicity)
+		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaaappy Holidays!"
 	end
 
 	def eat_milk_and_cookies(cookie_type)
 		puts "That was a good #{cookie_type}!"
-	end
-
-	def initialize
-		puts "Initializing Santa instance..."
 	end
 
 end
