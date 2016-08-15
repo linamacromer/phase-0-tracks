d81e555c22aba1f635f82f2f002b12396cba490d
Lina Cromer <linamacromer@gmail.com>
2016-08-15 16:19:16 -0700

    Add pseudocode for Dragon class

---
+++ ruby/solo.rb
@@ -0,0 +1,7 @@
+#Define a new class
+#at least three attributes (using at least two data types) whose values will vary from instance to instance (example: age)
+#at least three methods, at least one of which takes an argument
+
+#Dragon
+#attributes: color (str), height, weight, age (int), can_breathe_fire (bool)
+#method: roar several times (times parameter), breathes fire (must be can_breathe_fire true), eat (takes a full array of food items and eats them all to output empty array) 
