7fa14f9c16ce22f3bdccf6f0bc5d756a4a7788d8
Lina Cromer <linamacromer@gmail.com>
2016-08-15 09:46:52 -0700

    Add characteristics of Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,15 @@
+# SPECIES ------------------------
+# Puppus Adorableus
+
+# CHARACTERISTICS ----------------
+# Ear height: 5 inches
+# Friendly: yes
+# Good sense of smell: true
+# Name: varies
+# Bark volume: 8/10
+
+# BEHAVIOR -----------------------
+# Cuddle
+# Poop
+# Eat
+# Bark
