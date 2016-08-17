ea346744e2462c5e113ba9e8ff81e91a6ad46529
Lina Cromer <linamacromer@gmail.com>
2016-08-16 17:00:20 -0700

    Add yelling methods to Shout module, add driver code to test

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,17 @@
 module Shout
-	# we'll put some methods here soon, but this code is fine for now!
+
+	def self.yell_angrily(words)
+		words + "!!!" + ":-("
+	end
+
+	def self.yell_happily(words)
+		words + "!!!" + ":-D"
+	end
+
 end
+
+angry_phrase = "ugh"
+happy_phrase = "whee"
+
+p Shout.yell_angrily(angry_phrase)
+p Shout.yell_happily(happy_phrase)
