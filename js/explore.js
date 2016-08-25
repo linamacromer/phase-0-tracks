861bc708bda62fdbb3186ceffb6a3753c7df5f3e
Lina Cromer <linamacromer@gmail.com>
2016-08-25 11:55:34 -0700

    Refactor code and make DRY

--- js/explore.js
+++ js/explore.js
@@ -1,16 +1,16 @@
 // Take a string as a parameter and reverse string
 // Find the string's length and set the counter to the length
-// Run a loop that prints the counter index and subtracts 1 from counter until it reaches zero
-
-var str = "hello";
-var length = str.length - 1;
-var reversed = "";
+// Run a loop that adds the counter index to the reversed string and subtracts 1 from counter until it reaches zero
 
 function reverse(str) {
-	for (var i = length; i > -1; i+= -1) {
+	var reversed = "";
+	for (var i = str.length -1; i > -1; i+= -1) {
 		reversed = reversed + str[i];
 	}
 	console.log(reversed)
 }
 
-reverse(str)
+var reversed_str = reverse("hello");
+console.log(reversed_str)
+
+reverse("abcdefg")
