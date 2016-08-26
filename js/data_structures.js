e810a42a2040276f55d8265dda235dd3ebe7e58b
Lina Cromer <linamacromer@gmail.com>
2016-08-25 17:44:28 -0700

    Add car construction function, add driver code to test

--- js/data_structures.js
+++ js/data_structures.js
@@ -18,3 +18,25 @@ for (var i = 0; i < colors.length; i++) {
 }
 
 console.log(horses)
+
+function Car(make,model,color,year) {
+	console.log("Initializing car construction...");
+	this.make = make;
+	this.model = model;
+	this.color = color;
+	this.year = year;
+	this.honk = function() {
+		console.log("*HONK!!*");
+	}
+	console.log("Car complete!");
+}
+
+var car1 = new Car("Subaru", "Legacy", "Red", 2013);
+console.log(car1);
+car1.honk();
+console.log("-------");
+
+var car2 = new Car("Volkswagon","Jetta", "White", 2010);
+console.log(car2);
+car2.honk();
+console.log("-------");
