a3305b85ad30eddeb7571f5eaea7ae86bd1a898b
Lina Cromer <linamacromer@gmail.com>
2016-08-26 09:16:18 -0700

    Add pseudocode for finding longest phrase

---
+++ js/algos.js
@@ -0,0 +1,6 @@
+//Write a function that takes an array of words or phrases and returns the lonest word or phrase in the array
+	//Take array as parameter
+		//Set longest_phrase variable to blank
+		//Loop through each array item with index counter
+		//Conditional statement: if length is longer than length of longest_phrase variable, replace variable. If not longer, move to the next
+		//Return longest_phrase variable
