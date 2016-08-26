0977c2c4b940c05978b0477f561e6e411d9fc94d
Lina Cromer <linamacromer@gmail.com>
2016-08-26 09:40:31 -0700

    Add longest_phrase function and driver code to test

--- js/algos.js
+++ js/algos.js
@@ -4,3 +4,19 @@
 		//Loop through each array item with index counter
 		//Conditional statement: if length is longer than length of longest_phrase variable, replace variable. If not longer, move to the next
 		//Return longest_phrase variable
+
+function longest_phrase(phrase_array) {
+	var longest_phrase = "";
+	for (var i = 0; i < phrase_array.length; i++) {
+		if (phrase_array[i].length > longest_phrase.length) {
+			longest_phrase = phrase_array[i]
+		}
+	};
+	return longest_phrase;
+}
+
+var phrase_array = ["long phrase","longest phrase","longer phrase"];
+console.log(longest_phrase(phrase_array))
+
+var phrase_array2 = ["short", "longish", "superduperlong"];
+console.log(longest_phrase(phrase_array2))
