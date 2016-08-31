3f2b11c170a0d34e5bf323a8513b8c139dde3236
Lina Cromer <linamacromer@gmail.com>
2016-08-31 13:57:37 -0700

    Add driver code to test addition of recipes and printing list

--- databases/Something Awesome/Cookbook.rb
+++ databases/Something Awesome/Cookbook.rb
@@ -1,5 +1,5 @@
 # Create a cookbook compilation, and a way for the user to randomly generate a recipe from the cookbook.
-# Database will have ID index number, name of recipe, and hyperlink
+# Database will have ID index number, name of recipe, whether it can be for lunch, whether it can be for dinner
 # User will be able to run a random generator
 # User will be able to add a new recipe to the cookbook 
 # Stretch goal: database will have another column for how many times a recipe has been picked, and generate random recipe method will increase that number by 1, so that the generator picks from recipes that have not been used as much.
@@ -12,13 +12,27 @@ create_table_cmd = <<-SQL
   CREATE TABLE IF NOT EXISTS recipes(
     id INTEGER PRIMARY KEY,
     name VARCHAR(255),
-    link VARCHAR(255)
+    link VARCHAR(255),
+    lunch BOOLEAN,
+    dinner BOOLEAN
   )
 SQL
 
 cookbook.execute(create_table_cmd)
 
-def add_recipe(db, name, link)
-  db.execute("INSERT INTO recipes (name, link) VALUES (?, ?)", [name, link])
+def add_recipe(db, name, lunch, dinner)
+  db.execute("INSERT INTO recipes (name, lunch, dinner) VALUES (?, ?, ?)", [name, lunch, dinner])
 end
 
+def list_recipes(db)
+  recipes = db.execute("SELECT * FROM recipes")
+  recipes.each do |recipe|
+  	puts "#{recipe[0]}. #{recipe[1]}"
+	end
+end
+
+## DRIVER CODE **
+add_recipe(cookbook, "Tacos", "false", "true")
+add_recipe(cookbook, "Pot roast", "true", "true")
+
+list_recipes(cookbook)
