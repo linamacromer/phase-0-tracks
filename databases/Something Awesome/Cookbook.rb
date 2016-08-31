2d32e6df5e1421f36b33f711e598fc4e9de39a24
Lina Cromer <linamacromer@gmail.com>
2016-08-31 13:26:50 -0700

    Initial commit, add pseudocode for project goals.

---
+++ databases/Something Awesome/Cookbook.rb
@@ -0,0 +1,5 @@
+# Create a cookbook compilation, and a way for the user to randomly generate a recipe from the cookbook.
+# Database will have ID index number, name of recipe, and hyperlink
+# User will be able to run a random generator
+# User will be able to add a new recipe to the cookbook 
+# Stretch goal: database will have another column for how many times a recipe has been picked, and generate random recipe method will increase that number by 1, so that the generator picks from recipes that have not been used as much.
